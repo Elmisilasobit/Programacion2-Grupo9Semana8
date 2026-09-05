8e53a7f3d733c9db3f74d1e1a3f9879645093cb2
GarayJulio <agustinmiranda11235@gmail.com>
2026-09-05 16:25:08 -0700

    Update Proveedor.java

--- Proveedor.java
+++ Proveedor.java
@@ -17,5 +17,5 @@ public class Proveedor {
                 + " - Empresa proveedor: " + empresa
                 + " - Modificado por Desarrollador J");
     }
-    // se ha agregado la solucion 
+    // se ha agregado la solucion .
 }
