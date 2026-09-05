9fdb8003d1324c8081cbef6c80966be34f6ebd68
GarayJulio <agustinmiranda11235@gmail.com>
2026-09-04 20:18:05 -0700

    Update Proveedor.java
    parte arreglada por J

--- Proveedor.java
+++ Proveedor.java
@@ -1,3 +1,5 @@
+// Modificado por Desarrollador J  Solucion a el problema 
+
 public class Proveedor {
     private int idProveedor;
     private String nombre;
@@ -12,6 +14,7 @@ public class Proveedor {
     public void mostrarDetalle() {
         System.out.println("Proveedor #" + idProveedor
                 + " - Nombre: " + nombre
-                + " - Empresa: " + empresa);
+                + " - Empresa proveedor: " + empresa
+                + " - Modificado por Desarrollador J");
     }
 }
