42a5f2305eae5048e64d4e7ca9fa1103991eb141
Samaria Gamez <samar@example.com>
2026-09-04 19:21:20 -0600

    feat creat clase Producto con atributos encapsulados

---
+++ .idea/src/Producto.java
@@ -0,0 +1,29 @@
+public class Producto {
+    // C: Atributos + Constructor
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(int id, String nombre, double precio, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    // D: Getters, Setters y toString
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+    public int getStock() { return stock; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setPrecio(double precio) { this.precio = precio; }
+    public void setStock(int stock) { this.stock = stock; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | " + nombre + " | $" + precio + " | Stock: " + stock;
+    }
+}
